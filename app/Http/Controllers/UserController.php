59d3c2dd0c75b8bedde2c997b7831046926641e7
Filipp <im@filipp.su>
2025-10-21 18:35:30 +0400

    changed something in docs

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,6 +14,7 @@ class UserController extends Controller
 {
     /**
      * @OA\Post(
+     *     operationId="signup",
      *     path="/api/user/signup",
      *     tags={"Пользователь"},
      *     summary="Зарегистрировать нового пользователя",
